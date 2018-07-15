384b2b53bb41db44c45ed40fd36fa7b3c5c12464
cbta258 <cbta.258@gmail.com>
2018-07-15 14:19:05 -0500

    Add mehtods in controller pics and views show, edit, form, index and new

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,2 +1,39 @@
 class PicsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_pics
+  end
+
+  def pic_params
+  end
 end
