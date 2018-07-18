d295d58a636c571155150775f48989f71e99fa42
cbta258 <cbta.258@gmail.com>
2018-07-17 22:43:53 -0500

    Add functionality of pics controller and added strong parameters to application controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,14 +1,16 @@
 class PicsController < ApplicationController
+  before_action :find_pics, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order("created_at DESC")
   end
 
   def new
-    @pic = Pic.new
+    @pic = current_user.pics.build
   end
 
   def create
-    @pic = Pic.new(pic_params)
+    @pic = current_user.pics.build(pic_params)
 
     if @pic.save
       redirect_to @pic
@@ -18,22 +20,36 @@ class PicsController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
+
   end
 
   def update
+    @pic = Pic.update(pic_params)
+
+    if @pic.save
+      redirect_to @pic
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @pic.destroy
+
+    redirect_to root_path
   end
 
   private
 
   def find_pics
+    @pic = Pic.find(params[:id])
   end
 
   def pic_params
+    params.require(:pic).permit(:title, :description)
   end
 end
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,2 +1,5 @@
 class Pic < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
 end
